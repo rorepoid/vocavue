8a8db6d6ae65ae36b036c04519fd4648df41163d
Ritoooo <ritorito141516@gmail.com>
2019-11-18 02:48:00 -0500

    Hacemos que se añadan frutas y se calcule la cantidad total de frutas

--- public/01.js
+++ public/01.js
@@ -1,18 +1,20 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'PIAPRO',
+        titulo: 'VocaDB',
         frutas: [
             {nombre: 'Pera', cantidad: 10},
             {nombre: 'Manzana', cantidad: 0},
             {nombre: 'Plátano', cantidad: 11},
         ],
-        nuevaFruta: {nombre: null, cantidad: null}
+        nuevaFruta: {nombre: null, cantidad: 0},
+        total: 0,
     },
     methods: {
         agregarFruta() {
-            console.log(this.frutas[0].nombre);
-            this.frutas.push({nombre: this.nuevaFruta.nombre, cantidad: this.nuevaFruta.cantidad});
+            this.frutas.push({nombre: this.nuevaFruta.nombre, cantidad: parseInt(this.nuevaFruta.cantidad)});
+            this.nuevaFruta = {nombre: null, cantidad: 0};
+            this.total = this.frutas.reduce((contador, array) => contador + array.cantidad, 0);
         },
     }
 
