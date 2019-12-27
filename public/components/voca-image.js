26c69a2d84a0bb47dab4c21f2095f5b8363386e4
Roberto <roberto.orellana@glr.pe>
2019-12-26 20:03:01 -0500

    use second option of data

--- public/components/voca-image.js
+++ public/components/voca-image.js
@@ -3,10 +3,10 @@ Vue.component("voca-image", {
     template: `
             <div class="mx-0 sm:mx-1 md:mx-4">
                 <div class="bg-gray-600 rounded-lg h-56">
-                    <a :href="src.pvs[0].url"
+                    <a :href="src.pvs[1].url"
                     	target="_blank">
 	                    <img class="rounded-lg w-full h-full"
-	                         :src="src.pvs[0].thumbUrl" 
+	                         :src="src.pvs[1].thumbUrl" 
 	                         data-reactid="37"
 	                         onerror="this.src='img/aimaina.png'">
                      </a>
