11f5875eff2d566ef2fee64fa76c2ed24123edeb
Roberto <ritorito.141516@gmail.com>
2019-12-27 20:39:11 -0500

    search pvOriginal

--- public/components/voca-image.js
+++ public/components/voca-image.js
@@ -1,19 +1,31 @@
 Vue.component("voca-image", {
-	props: ['src'],
+	props: ['song'],
+    data(){
+        meta = {}
+        this.song.pvs.forEach((pv, index) => {
+            if (pv.pvType == "Original"){
+                meta.href = pv.url
+                meta.src = pv.thumbUrl
+                meta.author = pv.author
+                return
+            }
+        })
+        return meta
+    },
     template: `
         <div class="mx-0 sm:mx-1 md:mx-4">
             <div class="bg-gray-600 rounded-lg h-56">
-                <a :href="src.pvs[1].url"
+                <a :href="href"
                     target="_blank">
                     <img class="rounded-lg w-full h-full"
-                         :src="src.pvs[1].thumbUrl" 
+                         :src="src" 
                          data-reactid="37"
                          onerror="this.src='img/aimaina.png'">
                  </a>
             </div>
             <div class="text-left h-16">
-                <h1 class="">{{src.name}}</h1>
-                <p>{{src.pvs[1].author}}</p>
+                <h1 class="">{{song.name}}</h1>
+                <p>{{author}}</p>
             </div>
         </div>
     `,
