a0db53de0c8ec830b9ea9190135dbddcb5a879a3
Roberto Orellana <rorepoid@gmail.com>
2021-05-30 03:01:21 -0500

    style: Prettier (#16)

--- components/sidebar/MainSideBar.vue
+++ components/sidebar/MainSideBar.vue
@@ -1,41 +1,41 @@
 <template>
-    <div>
-        <div v-for="(option, index) of options" :key="index">
-            <side-bar-button :option="option"></side-bar-button>
-        </div>
+  <div>
+    <div v-for="(option, index) of options" :key="index">
+      <side-bar-button :option="option"></side-bar-button>
     </div>
+  </div>
 </template>
 
 <script>
-import SideBarButton from './SideBarButton.vue'
+import SideBarButton from "./SideBarButton.vue";
 
 export default {
   components: { SideBarButton },
-    data() {
-        return {
-            options: [
-                {
-                    text: "Inicio",
-                    link: "#Inicio",
-                },
-                {
-                    text: "Novedades",
-                    link: "#Novedades",
-                },
-                {
-                    text: "Favoritos",
-                    link: "#Favoritos",
-                },
-                {
-                    text: "Suscripciones",
-                    link: "#Suscripciones",
-                },
-                {
-                    text: "Artistas",
-                    link: "#Artistas",
-                },
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      options: [
+        {
+          text: "Inicio",
+          link: "#Inicio",
+        },
+        {
+          text: "Novedades",
+          link: "#Novedades",
+        },
+        {
+          text: "Favoritos",
+          link: "#Favoritos",
+        },
+        {
+          text: "Suscripciones",
+          link: "#Suscripciones",
+        },
+        {
+          text: "Artistas",
+          link: "#Artistas",
+        },
+      ],
+    };
+  },
+};
 </script>
