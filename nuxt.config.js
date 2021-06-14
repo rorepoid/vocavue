e03fc9bfec57f218640c1361addb0f387187569f
Roberto Orellana <rorepoid@gmail.com>
2021-06-13 23:00:08 -0500

    build: Install nuxt-svg-loader (#16)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,24 @@
 export default {
-    buildModules: ['@nuxtjs/tailwindcss'],
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-        ]
-    },
-    /*
-    ** Global CSS
-    */
-    css: ['./assets/styles/common.css'],
-}
+  buildModules: ["@nuxtjs/tailwindcss"],
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+  },
+  /*
+   ** Global CSS
+   */
+  css: ["./assets/styles/common.css"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ["nuxt-svg-loader"],
+};
