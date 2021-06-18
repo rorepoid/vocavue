2dcdd7f8cb7fda2b8d0709680a2c2259bb6a6e92
Roberto <rorepoid@gmail.com>
2021-06-18 10:50:12 -0500

    feat: Set song list columns min 10rem (#19)

--- pages/index.vue
+++ pages/index.vue
@@ -76,7 +76,7 @@ main {
 
 .song-list {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
   gap: 1em;
 }
 </style>
