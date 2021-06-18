74dba2e09a0113e6c01cc7e3114c934fa0323e63
Roberto <rorepoid@gmail.com>
2021-06-18 10:44:55 -0500

    refactor: Set aspect-ratio for images

--- components/home/Vocaimage.vue
+++ components/home/Vocaimage.vue
@@ -1,42 +1,47 @@
 <template>
-    <div class="mx-0 sm:mx-1 md:mx-4">
-        <div class="bg-gray-600 rounded-lg">
-            <a :href="href"
-                target="_blank">
-                <img class="rounded-lg w-full h-full"
-                        :src="src || require(`~/assets/aimaina.png`)"
-                        data-reactid="37"
-                >
-                </a>
-        </div>
-        <div class="text-left h-16">
-            <h1 class="">{{song.name}}</h1>
-            <p>{{author}}</p>
-        </div>
+  <article class="mx-0 rounded-lg text-white text-center song-card">
+    <a :href="href" target="_blank">
+      <img
+        class="rounded-lg w-full"
+        :src="src || require(`~/assets/aimaina.png`)"
+        data-reactid="37"
+      />
+    </a>
+    <div class="text-left h-16">
+      <h1 class="">{{ song.name }}</h1>
+      <p>{{ author }}</p>
     </div>
+  </article>
 </template>
 
 <script>
 export default {
-    props: ['song'],
+  props: ["song"],
 
-    data() {
-        return {
-            href: {},
-            src: {},
-            author: {},
-        }
-    },
+  data() {
+    return {
+      href: {},
+      src: {},
+      author: {},
+    };
+  },
 
-    mounted() {
-        this.song.pvs.forEach((pv, index) => {
-            if (pv.pvType == "Original"){
-                this.href = pv.url
-                this.src = pv.thumbUrl
-                this.author = pv.author
-                return
-            }
-        })
-    }
-}
+  mounted() {
+    this.song.pvs.forEach((pv, index) => {
+      if (pv.pvType == "Original") {
+        this.href = pv.url;
+        this.src = pv.thumbUrl;
+        this.author = pv.author;
+        return;
+      }
+    });
+  },
+};
 </script>
+
+<style scoped>
+img {
+  aspect-ratio: 12/9;
+  object-fit: contain;
+}
+</style>
